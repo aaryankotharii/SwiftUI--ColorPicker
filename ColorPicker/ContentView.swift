d43ffb111a127987658fb96ec839c31ce51e9e69
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-06 02:22:46 +0530

    initial commit

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -9,8 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var red : Double = 0
+    @State private var green : Double = 0
+    @State private var blue : Double = 0
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+  
+            Text("Hello, World!")
+        }
     }
 }
 
